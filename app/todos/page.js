80941d78c2b55dadaedd9ceb05f2dc4b1b103607
Byron Malana <malana2215277@ceu.edu.ph>
2025-04-28 17:52:14 +0800

    Initial Commit

---
+++ app/todos/page.js
@@ -0,0 +1,27 @@
+import prisma from "@/lib/prisma";
+import { Table,  TableHead, TableHeadCell, TableBody, TableRow, TableCell, Button } from "flowbite-react";
+import Link from "next/link";
+
+export default async function Todos() {
+    const todos = await prisma.todo.findMany({ include: { 
+        user: true 
+    }});
+    const tableRows = todos.map((todo) => <TableRow>
+        <TableCell><Link href={`/todos/${todo.id}`} className="text-blue-500">{todo.id}</Link></TableCell>
+        <TableCell>{todo.task}</TableCell>
+        <TableCell>{todo.user.username}</TableCell>
+    </TableRow>)
+    return <div>
+        <Button href="/todos/new" as={Link}>New</Button>
+        <Table>
+            <TableHead>
+                <TableHeadCell>ID</TableHeadCell>
+                <TableHeadCell>Todo</TableHeadCell>
+                <TableHeadCell>Owner</TableHeadCell>
+            </TableHead>
+            <TableBody>
+                {tableRows}
+            </TableBody>
+        </Table>
+    </div>
+}
